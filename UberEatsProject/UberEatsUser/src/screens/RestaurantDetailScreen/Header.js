661c1a849ee042459c73068951e7462269c91a05
hbao0421 <hbao20000421@gmail.com>
2022-12-20 02:06:47 -0500

    fetch data

--- UberEatsProject/UberEatsUser/src/screens/RestaurantDetailScreen/Header.js
+++ UberEatsProject/UberEatsUser/src/screens/RestaurantDetailScreen/Header.js
@@ -1,13 +1,15 @@
 import {View,Text,Image} from 'react-native';
 import styles from './style';
+
+const DEFAULT_IMAGE = "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/uber-eats/restaurant2.jpeg";
 const RestaurantHeader = ({restaurant})=>{
     return(
         <View style={styles.page}>
-            <Image source={{uri:restaurant.image}} style={styles.image} ></Image>
+            <Image source={{uri:restaurant.image.startsWith("http")?restaurant.image:DEFAULT_IMAGE}} style={styles.image} ></Image>
 
             <View style={styles.container}>
                 <Text style={styles.title}>{restaurant.name}</Text>
-                <Text style={styles.subtitle}>${restaurant.deliveryFee} &#8226; {restaurant.minDeliveryTime}-{restaurant.maxDeliveryTime} minutes</Text>
+                <Text style={styles.subtitle}>${restaurant.deliveryFee.toFixed(1)} &#8226; {restaurant.minDeliveryTime}-{restaurant.maxDeliveryTime} minutes</Text>
                 <Text style={styles.menuTitle}>Menu</Text>
             </View>
         </View>
--- UberEatsProject/UberEatsUser/src/screens/RestaurantDetailScreen/index.js
+++ UberEatsProject/UberEatsUser/src/screens/RestaurantDetailScreen/index.js
@@ -1,22 +1,34 @@
-import {View,FlatList} from 'react-native';
-import restaurants from "../../../assets/data/restaurants.json";
+import {View,FlatList,ActivityIndicator} from 'react-native';
+import { useState,useEffect } from 'react';
 import {Ionicons} from "@expo/vector-icons"
 import DishListItem from '../../components/DishListItem';
 import Header from './Header';
 import styles from './style';
-import {useRoute , useNavigation} from '@react-navigation/native'
-const restaurant = restaurants[0];
+import {useRoute , useNavigation} from '@react-navigation/native';
+import {DataStore} from 'aws-amplify';
+import {Restaurant,Dish} from '../../models';
+
 const ResaurantDetailsPage = ()=>{
+    const [restaurant,setRestaurant] = useState(null);
+    const [dishes,setDishes] = useState([]);
     const route = useRoute();
     const navigation = useNavigation();
     const id = route.params.id;
     // console.warn(id);
+    useEffect(()=>{
+        DataStore.query(Restaurant,id).then(setRestaurant);
+        DataStore.query(Dish,(dish)=>dish.restaurantID.eq(id)).then(setDishes);
+        // console.log(dishes.length);
+    },[]);
+    if(!restaurant){
+        return <ActivityIndicator size={"large"} color="gray"/>
+    }
 
     return(
         <View style={styles.page}>    
             <FlatList 
             ListHeaderComponent={() =><Header restaurant={restaurant}/>} 
-            data={restaurant.dishes} 
+            data={dishes} 
             renderItem={({item}) =><DishListItem dish={item}
             keyExtractor={(item)=>item.name}
             />}>
